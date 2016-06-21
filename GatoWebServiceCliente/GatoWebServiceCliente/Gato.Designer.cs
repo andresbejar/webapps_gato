65b13a7cb9afc4a633fb72ff0348c2cedfe0cb95
jeffryvenegas <jeffvene@gmail.com>
2016-06-21 10:50:50 -0600

    Falta guardar puntos en archivo

---
+++ GatoWebServiceCliente/GatoWebServiceCliente/Gato.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GatoWebServiceCliente
+{
+    partial class Gato
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gato));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.puntuaciones = new System.Windows.Forms.Button();
+            this.juegoNuevo = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.mensaje = new System.Windows.Forms.TextBox();
+            this.turnoText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 84);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 280);
+            this.panel1.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(2, 187);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(113, 84);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(122, 187);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(113, 84);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(240, 187);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(113, 84);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 84);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(121, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 84);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(240, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 84);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(240, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 84);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(122, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 84);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // puntuaciones
+            // 
+            this.puntuaciones.Location = new System.Drawing.Point(28, 307);
+            this.puntuaciones.Name = "puntuaciones";
+            this.puntuaciones.Size = new System.Drawing.Size(84, 43);
+            this.puntuaciones.TabIndex = 9;
+            this.puntuaciones.Text = "Puntuaciones";
+            this.puntuaciones.UseVisualStyleBackColor = true;
+            this.puntuaciones.Click += new System.EventHandler(this.puntuaciones_Click);
+            // 
+            // juegoNuevo
+            // 
+            this.juegoNuevo.Location = new System.Drawing.Point(147, 307);
+            this.juegoNuevo.Name = "juegoNuevo";
+            this.juegoNuevo.Size = new System.Drawing.Size(84, 43);
+            this.juegoNuevo.TabIndex = 10;
+            this.juegoNuevo.Text = "Juego Nuevo";
+            this.juegoNuevo.UseVisualStyleBackColor = true;
+            this.juegoNuevo.Click += new System.EventHandler(this.juegoNuevo_Click);
+            // 
+            // mensaje
+            // 
+            this.mensaje.BackColor = System.Drawing.SystemColors.Control;
+            this.mensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mensaje.Location = new System.Drawing.Point(253, 317);
+            this.mensaje.Name = "mensaje";
+            this.mensaje.ReadOnly = true;
+            this.mensaje.Size = new System.Drawing.Size(218, 22);
+            this.mensaje.TabIndex = 11;
+            // 
+            // turnoText
+            // 
+            this.turnoText.Location = new System.Drawing.Point(377, 78);
+            this.turnoText.Name = "turnoText";
+            this.turnoText.ReadOnly = true;
+            this.turnoText.Size = new System.Drawing.Size(101, 20);
+            this.turnoText.TabIndex = 12;
+            this.turnoText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.turnoText.TextChanged += new System.EventHandler(this.turnoText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Jugador 1 = \' O \'";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Jugador 2 = \' X \'";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Turno";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Gato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 357);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.turnoText);
+            this.Controls.Add(this.mensaje);
+            this.Controls.Add(this.juegoNuevo);
+            this.Controls.Add(this.puntuaciones);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Gato";
+            this.Text = "Gato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+       }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button puntuaciones;
+        private System.Windows.Forms.Button juegoNuevo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox mensaje;
+        private System.Windows.Forms.TextBox turnoText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GatoWebServiceCliente/GatoWebServiceCliente/Gato.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace GatoWebServiceCliente
+{
+    public partial class Gato : Form
+    {
+        private TITANIC.webapps_gatoPortClient gato;
+        private int turno;
+        private Stopwatch time1;
+        private Stopwatch time2;
+        private List <string> puntajes;
+        public Gato()
+        {
+            puntajes = new List <string>();            
+            InitializeComponent();
+            turno = 0;  // turno empieza con 0 = 'O'
+            turnoText.Text = "Jugador 1";
+            time1 = new Stopwatch();
+            time2 = new Stopwatch();
+            time1.Start();
+            time2.Reset();
+            gato = new TITANIC.webapps_gatoPortClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 0);
+            cambiarTurno();
+            if (result == "GAME OVER")
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button1.Enabled = false;
+            }
+
+        }
+
+        private void cambiarTurno()
+        {
+            if (turno == 0)
+            {
+                time1.Stop();
+                turno = 1;
+                time2.Start();
+            }
+            else
+            {
+                time2.Stop();
+                turno = 0;
+                time1.Start();
+            }
+            int jugador = turno + 1;
+            turnoText.Text = "Jugador " + (jugador).ToString();
+        }
+
+        private void mostrarGanador()
+        {
+            time1.Stop();
+            time2.Stop();
+            mensaje.BackColor = System.Drawing.Color.LightGreen;
+            int ganador = Int32.Parse(gato.getWinner())+1;
+            string winnerTime;
+            if (ganador == 1) {
+                winnerTime = String.Format("{0:00}", time1.Elapsed.Seconds);
+            }
+            else {
+                winnerTime = String.Format("{0:00}", time2.Elapsed.Seconds);
+            }
+            mensaje.Text = "Ganó jugador " + (ganador)+ " en: "+ winnerTime +"s";
+            String score = winnerTime + "s" + "    " + "jugador " + ganador;
+            puntajes.Add(score);
+            puntajes.Sort();
+            string top10 = "";
+            int length = puntajes.Count;
+            if(length > 10)
+            {
+                length = 10;
+            }
+            for(int i = 0; i < length; i++)
+            {
+                top10 += puntajes[i] + "\n";
+            }
+            gato.saveScores(top10);            
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 1);
+            cambiarTurno();
+            if (result == "GAME OVER")
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button2.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 2);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button3.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 3);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button4.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 4);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button5.Enabled = false;
+            }
+        }
+
+        private void puntuaciones_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void mostrarEstado(string[] board)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == "0")
+                {
+                    board[i] = "O";
+                }
+                else if (board[i] == "1")
+                {
+                    board[i] = "X";
+                }
+                else
+                {
+                    board[i] = "";
+                }
+            }
+            button1.Text = board[0];
+            button2.Text = board[1];
+            button3.Text = board[2];
+            button4.Text = board[3];
+            button5.Text = board[4];
+            button6.Text = board[5];
+            button7.Text = board[6];
+            button8.Text = board[7];
+            button9.Text = board[8];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 5);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button6.Enabled = false;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 6);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button7.Enabled = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 7);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button8.Enabled = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string result = gato.play(turno, 8);
+            cambiarTurno();
+            if (result == "GAME OVER")   
+            {
+                mostrarEstado(gato.getBoard().Split(','));
+                mostrarGanador();
+            }
+            else
+            {
+                mostrarEstado(result.Split(','));
+                button9.Enabled = false;
+            }
+        }
+
+        private void juegoNuevo_Click(object sender, EventArgs e)
+        {
+
+            gato.newGame();
+            if (turno == 0)
+            {
+                time1.Restart();
+                time2.Reset();
+            }
+            else
+            {
+                time2.Restart();
+                time1.Reset();
+            }
+            button1.Enabled = true;
+            button1.Text = "";
+            button2.Enabled = true;
+            button2.Text = "";
+            button3.Enabled = true;
+            button3.Text = "";
+            button4.Enabled = true;
+            button4.Text = "";
+            button5.Enabled = true;
+            button5.Text = "";
+            button6.Enabled = true;
+            button6.Text = "";
+            button7.Enabled = true;
+            button7.Text = "";
+            button8.Enabled = true;
+            button8.Text = "";
+            button9.Enabled = true;
+            button9.Text = "";
+            mensaje.Text = "";
+            mensaje.BackColor = System.Drawing.SystemColors.Control;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void turnoText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
